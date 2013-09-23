666c4f1ed642258834ab538e7d278ee2fce3ba6c
mlichtenberg <mlichtenberg@hotmail.com>
2013-09-23 10:37:44 -0500

    Updated SegmentSelectByStatusID to allow selection of unindexed segments.

--- Database-BHL/dbo/Stored Procedures/SegmentSelectByStatusID.sql
+++ Database-BHL/dbo/Stored Procedures/SegmentSelectByStatusID.sql
@@ -8,19 +8,36 @@ BEGIN
 
 SET NOCOUNT ON
 
+CREATE TABLE #tmpSegment
+	(
+	SegmentID int NOT NULL,
+	ItemID int NULL,
+	Title nvarchar(2000) NOT NULL,
+	Date nvarchar(20) NOT NULL,
+	Authors nvarchar(max) NULL,
+	SegmentClusterID int NULL,
+	DOIName nvarchar(50) NULL
+	)
+
+INSERT	#tmpSegment
 SELECT DISTINCT
 		s.SegmentID,
 		s.ItemID,
 		s.Title,
 		s.Date,
-		REPLACE(cat.Authors, '|', ' ') AS Authors,
+		REPLACE(cat.Authors, '|', ' '),
 		NULL AS SegmentClusterID,
 		d.DOIName
-INTO	#tmpSegment
 FROM	dbo.Segment s
-		INNER JOIN dbo.SearchCatalogSegment cat ON s.SegmentID = cat.SegmentID
+		LEFT JOIN dbo.SearchCatalogSegment cat ON s.SegmentID = cat.SegmentID
 		LEFT JOIN dbo.DOI d ON s.SegmentID = d.EntityID AND d.DOIEntityTypeID = 40
 WHERE	SegmentStatusID = @SegmentStatusID
+
+-- If we didn't get author information from the search catalog, attempt to compile it on-the-fly.
+-- This allows us to pick up author names for segments not yet indexed for searching.
+UPDATE	#tmpSegment
+SET		Authors = ISNULL(dbo.fnAuthorStringForSegment(SegmentID, ' ') + ' ', '')
+WHERE	Authors IS NULL
 
 UPDATE	#tmpSegment
 SET		SegmentClusterID = scs.SegmentClusterID
