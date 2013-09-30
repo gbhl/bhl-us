725c501c94ce929d95bebceb94c637a028a9985a
mlichtenberg <mlichtenberg@hotmail.com>
2013-09-30 17:14:08 -0500

    Added new OAIOLEF project to BHL solution.

---
+++ OAIOLEF/Convert.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MOBOT.BHL.OAI2;
+
+namespace MOBOT.BHL.OAIOLEF
+{
+    public class Convert
+    {
+        OAIRecord _oaiRecord;
+
+        public Convert(OAIRecord oaiRecord)
+        {
+            _oaiRecord = oaiRecord;
+        }
+
+        #region ToString
+
+        public new String ToString()
+        {
+            throw new NotImplementedException();
+
+            StringBuilder sb = new StringBuilder();
+
+
+
+
+
+            return sb.ToString();
+        }
+
+        #endregion ToString
+    }
+}
