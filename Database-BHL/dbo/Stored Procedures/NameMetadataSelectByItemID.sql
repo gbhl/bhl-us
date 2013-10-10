cff999db26f43da913f22f57ab38e9f7a7f02078
mlichtenberg <mlichtenberg@hotmail.com>
2013-10-10 14:38:07 -0500

    Corrected performance problem with NameMetadataSelectByItemID procedure.

--- Database-BHL/dbo/Stored Procedures/NameMetadataSelectByItemID.sql
+++ Database-BHL/dbo/Stored Procedures/NameMetadataSelectByItemID.sql
@@ -10,15 +10,32 @@ SET NOCOUNT ON
 SELECT	p.PageID,
 		p.SequenceOrder,
 		dbo.fnIndicatedPageStringForPage(p.PageID) AS IndicatedPages,
-		dbo.fnPageTypeStringForPage(p.PageID) AS PageTypes,
-		nr.NameResolvedID,
-		nr.ResolvedNameString
+		dbo.fnPageTypeStringForPage(p.PageID) AS PageTypes
+INTO	#tmpPage
 FROM	dbo.Page p WITH (NOLOCK)
-		LEFT JOIN NamePage np WITH (NOLOCK) ON p.PageID = np.PageID
-		LEFT JOIN Name n WITH (NOLOCK) ON np.NameID = n.NameID
-		LEFT JOIN NameResolved nr WITH (NOLOCK) ON n.NameResolvedID = nr.NameResolvedID
 WHERE	p.ItemID = @ItemID
 AND		p.Active = 1
-ORDER BY
-		p.SequenceOrder, p.PageID, nr.ResolvedNameString ASC
 
+-- Get Name info
+SELECT	p.PageID,
+		p.SequenceOrder,
+		nr.NameResolvedID,
+		nr.ResolvedNameString
+INTO	#tmpName
+FROM	#tmpPage p WITH (NOLOCK)
+		INNER JOIN NamePage np WITH (NOLOCK) ON p.PageID = np.PageID
+		INNER JOIN Name n WITH (NOLOCK) ON np.NameID = n.NameID
+		INNER JOIN NameResolved nr WITH (NOLOCK) ON n.NameResolvedID = nr.NameResolvedID
+
+-- Combine page and name information
+SELECT DISTINCT
+		p.PageID,
+		p.SequenceOrder,
+		p.IndicatedPages,
+		p.PageTypes,
+		n.NameResolvedID,
+		n.ResolvedNameString
+FROM	#tmpPage p
+		LEFT JOIN #tmpName n on p.PageID = n.PageID
+ORDER BY
+		p.SequenceOrder, p.PageID, n.ResolvedNameString ASC
