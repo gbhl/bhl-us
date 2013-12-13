74b775c888e08e7e1eeb1f73bb4e72acde5c57fc
mlichtenberg <mlichtenberg@hotmail.com>
2013-12-13 16:34:24 -0600

    Updated PageDetailHarvest to allow re-harvesting of data.

---
+++ Database-BHL/dbo/Stored Procedures/PageDetailInsertUpdate.sql
@@ -0,0 +1,91 @@
+﻿CREATE PROCEDURE [dbo].[PageDetailInsertUpdate]
+
+@PageID int,
+@PageDetailStatusID int,
+@Height int,
+@Width int,
+@PixelDepth int,
+@AbbyyHasImage smallint,
+@ContrastHasImage smallint,
+@PercentCoverage decimal(5,2)
+
+AS
+
+BEGIN
+
+SET NOCOUNT ON
+
+DECLARE @PageDetailID int
+SET @PageDetailID = NULL
+
+SELECT @PageDetailID = PageDetailID FROM dbo.PageDetail WHERE PageID = @PageID
+
+IF @PageDetailID IS NULL
+BEGIN
+	-- No prior record, so insert a new record
+	INSERT	dbo.PageDetail
+		(
+		PageID,
+		PageDetailStatusID,
+		Height,
+		Width,
+		PixelDepth,
+		AbbyyHasImage,
+		ContrastHasImage,
+		PercentCoverage
+		)
+	VALUES
+		(
+		@PageID,
+		@PageDetailStatusID,
+		@Height,
+		@Width,
+		@PixelDepth,
+		@AbbyyHasImage,
+		@ContrastHasImage,
+		@PercentCoverage
+		)
+
+	SELECT @PageDetailID = SCOPE_IDENTITY()
+END
+ELSE
+BEGIN
+	-- Update the existing record
+	UPDATE	dbo.PageDetail
+	SET		PageDetailStatusID = 10,	-- Extracted
+			Height = @Height,
+			Width = @Width,
+			PixelDepth = @PixelDepth,
+			AbbyyHasImage = @AbbyyHasImage,
+			ContrastHasImage = @ContrastHasImage,
+			PercentCoverage = @PercentCoverage,
+			LastModifiedDate = GETDATE(),
+			LastModifiedUserID = 1
+	WHERE	PageDetailID = @PageDetailID
+
+	-- Remove any attached PageIllustration records (they will be replaced)
+	DELETE FROM dbo.PageIllustration WHERE PageDetailID = @PageDetailID
+END
+
+-- Return the new/updated record
+SELECT	PageDetailID,
+		PageID,
+		PageDetailStatusID,
+		StatusDate,
+		Height,
+		Width,
+		PixelDepth,
+		AbbyyHasImage,
+		ContrastHasImage,
+		PercentCoverage,
+		CreationDate,
+		LastModifiedDate,
+		CreationUserID,
+		LastModifiedUserID
+FROM	dbo.PageDetail
+WHERE	PageDetailID = @PageDetailID
+
+END
+
+GO
+
