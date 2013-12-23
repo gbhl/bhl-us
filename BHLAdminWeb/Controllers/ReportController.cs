527e7aa3bd1b29966fc209fba211a6453498bf2f
MOBOT\mlichtenberg <MOBOT\mlichtenberg>
2013-12-23 15:58:09 -0600

    Updated MVC sample models/views/controllers.

--- BHLAdminWeb/Controllers/ReportController.cs
+++ BHLAdminWeb/Controllers/ReportController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MOBOT.BHL.AdminWeb.Models;
 
 namespace MOBOT.BHL.AdminWeb.Controllers
 {
@@ -13,7 +14,13 @@ namespace MOBOT.BHL.AdminWeb.Controllers
 
         public ActionResult SegmentResolutionLog()
         {
-            return View();
+            ViewBag.PageTitle = "Segment Resolution Log";
+
+            SegmentResolutionLogModel model = new SegmentResolutionLogModel();
+            model.SegmentResolutionLogs.Add("Log ID: 1, Segment ID: 73279, Matching Segment ID: 69421, Score: 0.8618");
+            model.SegmentResolutionLogs.Add("Log ID: 2, Segment ID: 73279, Matching Segment ID: 73310, Score: 0.6243");
+
+            return View(model);
         }
 
     }
---
+++ BHLAdminWeb/Models/SegmentResolutionLogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MOBOT.BHL.AdminWeb.Models
+{
+    public class SegmentResolutionLogModel
+    {
+        private List<string> _segmentResolutionLogs = new List<string>();
+
+        public List<string> SegmentResolutionLogs
+        {
+            get { return _segmentResolutionLogs; }
+            set { _segmentResolutionLogs = value; }
+        }        
+    }
+}
