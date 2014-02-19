7bbfb281172590736b647ff72fbb644b7b8122e3
mlichtenberg <mlichtenberg@hotmail.com>
2014-02-19 17:00:42 -0600

    Revised ItemSelectNonMemberMonograph procedure to improve efficiency

--- Database-BHL/dbo/Stored Procedures/ItemSelectNonMemberMonograph.sql
+++ Database-BHL/dbo/Stored Procedures/ItemSelectNonMemberMonograph.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROCEDURE [dbo].[ItemSelectNonMemberMonograph]
+﻿CREATE PROCEDURE [dbo].[ItemSelectNonMemberMonograph]
 
 @SinceDate datetime,
 @IsMember int = 0,
@@ -11,12 +10,21 @@ BEGIN
 
 SET NOCOUNT ON
 
-SELECT	DISTINCT 
-		t.TitleID,
+SELECT	i.ItemID,
+		i.PrimaryTitleID,
+		i.InstitutionCode,
+		i.Volume,
+		i.IdentifierBib,
+		c.Authors
+INTO	#Item
+FROM	dbo.Item i INNER JOIN dbo.SearchCatalog c ON i.ItemID = c.ItemID
+WHERE	i.CreationDate > @SinceDate
+AND		c.HasLocalContent = 1
+
+SELECT	t.TitleID,
 		ISNULL(tti.IdentifierValue, '') AS OCLC,
 		t.FullTitle,
-		--dbo.fnAuthorStringForTitle(t.TitleID) AS Authors,
-		c.Authors,
+		i.Authors,
 		ISNULL(CONVERT(nvarchar(10), t.StartYear), '') AS StartYear,
 		t.CallNumber,
 		t.DataField_260_b AS Publisher,
@@ -24,19 +32,16 @@ SELECT	DISTINCT
 		i.ItemID,
 		i.Volume,
 		i.IdentifierBib
-FROM	dbo.Title t INNER JOIN dbo.Item i ON t.TitleID = i.PrimaryTitleID
+FROM	#Item i
+		INNER JOIN dbo.Title t ON i.PrimaryTitleID = t.TitleID
 		INNER JOIN dbo.TitleItem ti ON t.TitleID = ti.TitleID AND ti.ItemID = i.ItemID
 		LEFT JOIN dbo.Institution inst ON i.InstitutionCode = inst.InstitutionCode
 		INNER JOIN dbo.BibliographicLevel b ON t.BibliographicLevelID = b.BibliographicLevelID
 		LEFT JOIN dbo.Title_Identifier tti ON t.TitleID = tti.TitleID AND tti.IdentifierID = 1 -- OCLC
-		INNER JOIN dbo.Page p ON ti.ItemID = p.ItemID
-		INNER JOIN dbo.SearchCatalog c ON i.ItemID = c.ItemID
 WHERE	ISNULL(inst.BHLMemberLibrary, 0) = @IsMember
 AND		(inst.InstitutionCode = @InstitutionCode OR @InstitutionCode = '')
 AND		b.MARCCode IN ('a', 'c', 'm') -- monographs and "collections"
-AND		i.CreationDate > @SinceDate
 ORDER BY
 		t.TitleID
 
 END
-
