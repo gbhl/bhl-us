0cefb048c651405907ba232715b784d44f2f292c
mlichtenberg <mlichtenberg@hotmail.com>
2014-09-30 16:42:23 -0500

    Cleaned up database project files and settings.

--- Database-BHL/Data/dbo.EntityCountType.Table.sql
+++ Database-BHL/Data/dbo.EntityCountType.Table.sql
@@ -1,6 +1,4 @@
-﻿USE BHL
-GO
-INSERT EntityCountType (EntityCountTypeID, FullName, DisplayName, Sequence) VALUES (1, N'Active Titles', N'titles', 1)
+﻿INSERT EntityCountType (EntityCountTypeID, FullName, DisplayName, Sequence) VALUES (1, N'Active Titles', N'titles', 1)
 INSERT EntityCountType (EntityCountTypeID, FullName, DisplayName, Sequence) VALUES (2, N'Active Items', N'volumes', 2)
 INSERT EntityCountType (EntityCountTypeID, FullName, DisplayName, Sequence) VALUES (3, N'Active Pages', N'pages', 3)
 INSERT EntityCountType (EntityCountTypeID, FullName, DisplayName, Sequence) VALUES (4, N'Active Segments', N'parts', 4)
