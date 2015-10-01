8c8f0a8f7106ab8b00a11348d2021df882296f13
mlichtenberg <mlichtenberg@hotmail.com>
2015-10-01 15:10:17 -0500

    Updated OpenURL methods to follow title and segment redirects

--- Database-BHL/dbo/Stored Procedures/OpenUrlCitationSelectBySegmentID.sql
+++ Database-BHL/dbo/Stored Procedures/OpenUrlCitationSelectBySegmentID.sql
@@ -9,6 +9,23 @@ BEGIN
 
 SET NOCOUNT ON
 
+-- If the segment has been redirected to a different segment, then use
+-- that segment instead.  Follow the "redirect" chain up to ten levels.
+SELECT	@SegmentID = COALESCE(s10.SegmentID, s9.SegmentID, s8.SegmentID, s7.SegmentID, s6.SegmentID,
+						s5.SegmentID, s4.SegmentID, s3.SegmentID, s2.SegmentID, s1.SegmentID)
+FROM	dbo.Segment s1
+		LEFT JOIN dbo.Segment s2 ON s1.RedirectSegmentID = s2.SegmentID
+		LEFT JOIN dbo.Segment s3 ON s2.RedirectSegmentID = s3.SegmentID
+		LEFT JOIN dbo.Segment s4 ON s3.RedirectSegmentID = s4.SegmentID
+		LEFT JOIN dbo.Segment s5 ON s4.RedirectSegmentID = s5.SegmentID
+		LEFT JOIN dbo.Segment s6 ON s5.RedirectSegmentID = s6.SegmentID
+		LEFT JOIN dbo.Segment s7 ON s6.RedirectSegmentID = s7.SegmentID
+		LEFT JOIN dbo.Segment s8 ON s7.RedirectSegmentID = s8.SegmentID
+		LEFT JOIN dbo.Segment s9 ON s8.RedirectSegmentID = s9.SegmentID
+		LEFT JOIN dbo.Segment s10 ON s9.RedirectSegmentID = s10.SegmentID
+WHERE	s1.SegmentID = @SegmentID
+
+
 CREATE TABLE #tmpOpenUrlCitation
 	(
 	PageID int NULL DEFAULT(0),
@@ -78,7 +95,6 @@ FROM	#tmpOpenUrlCitation t INNER JOIN SegmentIdentifier si WITH (NOLOCK)
 SELECT * FROM #tmpOpenUrlCitation ORDER BY SegmentTitle, Volume, Date, StartPage
 
 END
-
 
 
 
