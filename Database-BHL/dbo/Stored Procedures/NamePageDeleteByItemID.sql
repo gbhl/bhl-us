a871bc495a4d8c9eb692b9388c3b7fd2b94aeb30
mlichtenberg <mlichtenberg@hotmail.com>
2016-01-06 11:52:03 -0600

    Updated NamePageDeleteByItemID procedure to ignore user-defined records

--- Database-BHL/dbo/Stored Procedures/NamePageDeleteByItemID.sql
+++ Database-BHL/dbo/Stored Procedures/NamePageDeleteByItemID.sql
@@ -1,18 +1,22 @@
-﻿CREATE PROCEDURE dbo.NamePageDeleteByItemID
+﻿CREATE PROCEDURE [dbo].[NamePageDeleteByItemID]
 
 @ItemID int
 
 AS
----------------------------------------------------------------------------------------
--- Remove existing name records for an item, and reset the item/page name lookup dates.
----------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+-- Remove existing auto-added name records for an item, and reset the item/page name lookup dates.
+--------------------------------------------------------------------------------------------------
 BEGIN
+
+SET NOCOUNT ON
 
 DELETE	dbo.NamePage
 FROM	dbo.NamePage np INNER JOIN dbo.Page p ON np.PageID = p.PageID
 WHERE	p.ItemID = @ItemID
+AND		ISNULL(np.CreationUserID, 1) = 1
 
 UPDATE dbo.Page SET LastPageNameLookupDate = NULL WHERE ItemID = @ItemID
 UPDATE dbo.Item SET LastPageNameLookupDate = NULL WHERE ItemID = @ItemID
 
 END
+
