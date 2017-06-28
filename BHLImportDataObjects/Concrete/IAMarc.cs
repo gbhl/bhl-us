46caa257b0722fd55d4a517828c53dc8eda89b6e
mlichtenberg <mlichtenberg@hotmail.com>
2017-06-28 11:18:46 -0500

    Prevent truncation of MARC Leader values during data ingest

--- BHLImportDataObjects/Concrete/IAMarc.cs
+++ BHLImportDataObjects/Concrete/IAMarc.cs
@@ -1,7 +1,9 @@
 
 #region Using
 
+using CustomDataAccess;
 using System;
+using System.Data;
 
 #endregion Using
 
@@ -10,5 +12,28 @@ namespace MOBOT.BHLImport.DataObjects
 	[Serializable]
 	public class IAMarc : __IAMarc
 	{
-	}
+        /// <summary>
+        /// Override the base Leader property to avoid having the leading spaces 
+        /// trimmed off of the value when the property is set.
+        /// </summary>
+        private string _Leader = string.Empty;
+
+        [ColumnDefinition("Leader", DbTargetType = SqlDbType.VarChar, Ordinal = 3, CharacterMaxLength = 200)]
+        public new string Leader
+        {
+            get
+            {
+                return _Leader;
+            }
+            set
+            {
+                if (value != null) value = value.Substring(0, (value.Length > 200 ? 200 : value.Length));
+                if (_Leader != value)
+                {
+                    _Leader = value;
+                    _IsDirty = true;
+                }
+            }
+        }
+    }
 }
