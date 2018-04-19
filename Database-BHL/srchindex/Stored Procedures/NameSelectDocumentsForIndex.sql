e566773cd1d09a1b6ff2b112054ea958a1effe4c
mlichtenberg <mlichtenberg@hotmail.com>
2018-04-19 17:04:16 -0500

    Performance improvements in indexing stroed procedure

--- Database-BHL/srchindex/Stored Procedures/NameSelectDocumentsForIndex.sql
+++ Database-BHL/srchindex/Stored Procedures/NameSelectDocumentsForIndex.sql
@@ -10,16 +10,21 @@ BEGIN
 SET NOCOUNT ON
 
 SELECT TOP 200000
-		r.NameResolvedID,
+		NameResolvedID,
+		ResolvedNamestring
+INTO	#NameResolved
+FROM	dbo.NameResolved WITH (NOLOCK)
+WHERE	NameResolvedID >= @StartID
+AND		(NameResolvedID <= @EndID OR @EndID IS NULL)
+
+SELECT	r.NameResolvedID,
 		r.ResolvedNameString,
 		SUM(CASE WHEN i.ItemStatusID = 40 AND p.Active = 1 AND n.IsActive = 1 THEN 1 ELSE 0 END) AS NameCount
-FROM	dbo.NameResolved r WITH (NOLOCK)
+FROM	#NameResolved r
 		INNER JOIN dbo.Name n WITH (NOLOCK) ON r.NameResolvedID = n.NameResolvedID
 		LEFT JOIN dbo.NamePage np WITH (NOLOCK) ON n.NameID = np.NameID
 		LEFT JOIN dbo.Page p WITH (NOLOCK) ON np.PageID = p.PageID
 		LEFT JOIN dbo.Item i WITH (NOLOCK) ON p.ItemID = i.ItemID
-WHERE	r.NameResolvedID >= @StartID
-AND		(r.NameResolvedID <= @EndID OR @EndID IS NULL)
 GROUP BY r.NameResolvedID,
         r.ResolvedNameString
 ORDER BY r.NameResolvedID
