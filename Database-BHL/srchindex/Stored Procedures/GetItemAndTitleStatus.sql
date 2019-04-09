86142734548aea0bb29315873a19756ce366e499
mlichtenberg <mlichtenberg@hotmail.com>
2019-04-09 14:11:55 -0500

    Bug fixes to BHLSearchIndexer

---
+++ Database-BHL/srchindex/Stored Procedures/GetItemAndTitleStatus.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE srchindex.GetItemAndTitleStatus
+
+@TitleID int,
+@ItemID int
+
+AS
+
+BEGIN
+
+SET NOCOUNT ON
+
+SELECT	x.PublishReady,
+		CAST(CASE WHEN ItemStatusName = 'Published' THEN 1 ELSE 0 END AS BIT) AS IsPublished
+FROM	(
+		SELECT	t.PublishReady, i.ItemStatusID 
+		FROM	dbo.Title t CROSS JOIN dbo.Item i 
+		WHERE	t.TitleID = @TitleID
+		AND		i.ItemID = @ItemID
+		) x
+		INNER JOIN dbo.ItemStatus s on x.ItemStatusID = s.ItemStatusID
+
+END
---
+++ Database-BHL/srchindex/Stored Procedures/ItemSelectForTitle.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [srchindex].[ItemSelectForTitle]
+
+@TitleID int
+
+AS 
+
+BEGIN
+
+SET NOCOUNT ON
+
+-- Return IDs of the published items for the specified title
+SELECT	i.ItemID
+FROM	dbo.TitleItem ti 
+		INNER JOIN dbo.Item i ON ti.ItemID = i.ItemID
+		INNER JOIN dbo.Title t ON ti.TitleID = t.TitleID
+WHERE	ti.TitleID = @TitleID
+AND		i.ItemStatusID = 40
+
+END
