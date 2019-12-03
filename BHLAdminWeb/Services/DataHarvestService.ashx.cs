a03e098993ab9a6462ac913c91eea3d2a6c43734
mlichtenberg <mlichtenberg@hotmail.com>
2019-12-03 12:17:36 -0600

    Updated _blank links on the admin site

--- BHLAdminWeb/Services/DataHarvestService.ashx.cs
+++ BHLAdminWeb/Services/DataHarvestService.ashx.cs
@@ -136,7 +136,7 @@ namespace MOBOT.BHL.AdminWeb.Services
                 for (int x = 0; x < searchResult.Count; x++)
                 {
                     response.Append("<row id='" + searchResult[x].ItemId.ToString() + "'>");
-                    response.Append("<cell> <![CDATA[<a title=\"Info\" target=\"_blank\" href=\"http://www.archive.org/details/" + searchResult[x].IAIdentifier + "\">" + searchResult[x].IAIdentifier + "</a>]]> </cell>");
+                    response.Append("<cell> <![CDATA[<a title=\"Info\" rel=\"noopener noreferrer\" target=\"_blank\" href=\"http://www.archive.org/details/" + searchResult[x].IAIdentifier + "\">" + searchResult[x].IAIdentifier + "</a>]]> </cell>");
                     response.Append("<cell> " + SecurityElement.Escape(searchResult[x].Sponsor) + " </cell>");
                     response.Append("<cell> " + SecurityElement.Escape(searchResult[x].ScanningCenter) + " </cell>");
                     response.Append("<cell> " + SecurityElement.Escape(searchResult[x].Volume) + " </cell>");
@@ -203,7 +203,7 @@ namespace MOBOT.BHL.AdminWeb.Services
                 for (int x = 0; x < searchResult.Count; x++)
                 {
                     response.Append("<row id='" + searchResult[x].ItemId.ToString() + "'>");
-                    response.Append("<cell> <![CDATA[<a title=\"Info\" target=\"_blank\" href=\"/item/" + searchResult[x].BHLItemId + "\">" + searchResult[x].BHLItemId + "</a>]]> </cell>");
+                    response.Append("<cell> <![CDATA[<a title=\"Info\" rel=\"noopener noreferrer\" target=\"_blank\" href=\"/item/" + searchResult[x].BHLItemId + "\">" + searchResult[x].BHLItemId + "</a>]]> </cell>");
                     response.Append("<cell> " + SecurityElement.Escape(searchResult[x].Title) + " </cell>");
                     response.Append("<cell> " + SecurityElement.Escape(searchResult[x].Volume) + " </cell>");
                     response.Append("<cell> <![CDATA[<a href=\"#\" title=\"Segments\" onclick=\"window.open('BioStorSegmentsForItem.aspx?id=" + searchResult[x].ItemId.ToString() + "', 'Segments', 'resizeable=0,scrollbars=1,height=500,width=500,status=0,toolbar=0,menubar=0,location=0');\">" + searchResult[x].TotalSegments + "</a>]]> </cell>");
