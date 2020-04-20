8fb949d9d49245ebff0f67af76642bc7313126b7
mlichtenberg <mlichtenberg@hotmail.com>
2020-04-20 15:09:53 -0500

    Added job to reset pagination locks

---
+++ Database-BHL/dbo/Stored Procedures/ItemResetPaginationLock.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE dbo.ItemResetPaginationLock
+
+@MaxAge int
+
+AS
+
+BEGIN
+
+-- Resets 'In Progress' items to 'Incomplete' if they have been 'In Progress' longer than @MaxAge.
+-- 'In Progress' items are 'locked', and prevent any other user from paginating them.
+
+SET NOCOUNT ON
+
+DECLARE @Incomplete int
+DECLARE @InProgress int
+SELECT @Incomplete = PaginationStatusID FROM PaginationStatus WHERE PaginationStatusName = 'Incomplete'
+SELECT @InProgress = PaginationStatusID FROM PaginationStatus WHERE PaginationStatusName = 'In Progress'
+
+UPDATE	dbo.Item 
+SET		PaginationStatusID = @Incomplete,
+		PaginationStatusDate = GETDATE()
+WHERE	PaginationStatusID = @InProgress
+AND		DATEDIFF(day, PaginationStatusDate, GETDATE()) > @MaxAge
+
+END
