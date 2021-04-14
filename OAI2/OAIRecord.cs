2bf0e4df18f475c3077d826ec60f1a3b1027de68
mlichtenberg <mlichtenberg@hotmail.com>
2021-04-14 15:02:55 -0500

    Fixed item URLs in MODS files

--- OAI2/OAIRecord.cs
+++ OAI2/OAIRecord.cs
@@ -560,7 +560,7 @@ namespace MOBOT.BHL.OAI2
                     if (itemTitle.TitleID == book.PrimaryTitleID) this.Sequence = itemTitle.ItemSequence.ToString();
                 }
 
-                this.Url = "https://www.biodiversitylibrary.org/item/" + book.ItemID.ToString();
+                this.Url = "https://www.biodiversitylibrary.org/item/" + book.BookID.ToString();
                 this.ParentUrl = "https://www.biodiversitylibrary.org/bibliography/" + book.PrimaryTitleID.ToString();
                 if (book.ThumbnailPageID != null) this.ThumbnailUrl = "https://www.biodiversitylibrary.org/pagethumb/" + book.ThumbnailPageID.ToString();
 
