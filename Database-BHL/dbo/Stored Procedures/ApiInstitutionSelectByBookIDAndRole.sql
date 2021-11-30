9c5af2ee8f6ee17ee99555a053e3a23597362bcc
mlichtenberg <mlichtenberg@hotmail.com>
2021-11-30 14:03:44 -0600

    Corrected bug preventing RightsHolder from appearing in API responses

---
+++ Database-BHL/dbo/Stored Procedures/ApiInstitutionSelectByBookIDAndRole.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[ApiInstitutionSelectByBookIDAndRole]
+
+@BookID int,
+@InstitutionRoleName nvarchar(100)
+
+AS
+
+BEGIN
+
+SET NOCOUNT ON
+
+SELECT	i.InstitutionCode,
+		i.InstitutionName,
+		i.Note,
+		i.InstitutionUrl,
+		i.BHLMemberLibrary,
+		ii.ItemInstitutionID,
+		r.InstitutionRoleName,
+		r.InstitutionRoleLabel,
+		i.CreationDate,
+		i.LastModifiedDate,
+		i.CreationUserID,
+		i.LastModifiedUserID
+FROM	dbo.ItemInstitution ii
+		INNER JOIN dbo.Institution i ON ii.InstitutionCode = i.InstitutionCode
+		INNER JOIN dbo.InstitutionRole r ON ii.InstitutionRoleID = r.InstitutionRoleID
+		INNER JOIN dbo.Book b ON ii.ItemID = b.ItemID
+WHERE	b.BookID = @BookID
+AND		r.InstitutionRoleName = @InstitutionRoleName
+
+END
+
+GO
