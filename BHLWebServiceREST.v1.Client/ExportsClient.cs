6d478fd950a4084ef693c55f3b6d11c5d605a8e0
mlichtenberg <mlichtenberg@hotmail.com>
2022-06-02 18:24:10 -0500

    Revised ExportsClient to use connections more efficiently

--- BHLWebServiceREST.v1.Client/ExportsClient.cs
+++ BHLWebServiceREST.v1.Client/ExportsClient.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BHL.WebServiceREST.v1.Client
 {
     public class ExportsClient : RestClient
     {
+        private HttpClient _httpClient = null;
+
         public ExportsClient(string baseUrl) : base(baseUrl)
         {
+            _httpClient = GetHttpClient();
+        }
+
+        ~ExportsClient()
+        {
+            _httpClient.Dispose();
+            _httpClient = null;
         }
 
         public async Task<ICollection<TitleBibTeX>> GetTitleBibTexCitationsAsync()
@@ -72,164 +82,110 @@ namespace BHL.WebServiceREST.v1.Client
 
         public async Task<string> GetTitleMODSAsync(int titleID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetTitleMODSAsync(titleID).ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetTitleMODSAsync(titleID).ConfigureAwait(false));
         }
 
         public string GetTitleMODS(int titleID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetTitleMODS(titleID);
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetTitleMODS(titleID);
         }
 
         public async Task<string> GetItemMODSAsync(int itemID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetItemMODSAsync(itemID).ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetItemMODSAsync(itemID).ConfigureAwait(false));
         }
 
         public string GetItemMODS(int itemID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetItemMODS(itemID);
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetItemMODS(itemID);
         }
 
         public async Task<string> GetSegmentMODSAsync(int segmentID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetSegmentMODSAsync(segmentID).ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetSegmentMODSAsync(segmentID).ConfigureAwait(false));
         }
 
         public string GetSegmentMODS(int segmentID)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetSegmentMODS(segmentID);
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetSegmentMODS(segmentID);
         }
 
         public async Task<ICollection<RISCitation>> GetTitleRISCitationsAsync()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetTitleRISCitationsAsync().ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetTitleRISCitationsAsync().ConfigureAwait(false));
         }
 
         public ICollection<RISCitation> GetTitleRISCitations()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetTitleRISCitations();
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetTitleRISCitations();
         }
 
         public async Task<ICollection<RISCitation>> GetItemRISCitationsAsync()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetItemRISCitationsAsync().ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetItemRISCitationsAsync().ConfigureAwait(false));
         }
 
         public ICollection<RISCitation> GetItemRISCitations()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetItemRISCitations();
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetItemRISCitations();
         }
 
         public async Task<ICollection<RISCitation>> GetSegmentRISCitationsAsync()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetSegmentRISCitationsAsync().ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetSegmentRISCitationsAsync().ConfigureAwait(false));
         }
 
         public ICollection<RISCitation> GetSegmentRISCitations()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetSegmentRISCitations();
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetSegmentRISCitations();
         }
 
         public async Task<string> GetRISCitationStringAsync(RISCitation citation)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetRISCitationStringAsync(citation).ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetRISCitationStringAsync(citation).ConfigureAwait(false));
         }
 
         public string GetRISCitationString(RISCitation citation)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetRISCitationString(citation);
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetRISCitationString(citation);
         }
 
         public async Task<ICollection<string>> GetIAIdentifiersAsync()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetIAIdentifiersAsync().ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetIAIdentifiersAsync().ConfigureAwait(false));
         }
 
         public ICollection<string> GetIAIdentifiers()
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetIAIdentifiers();
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetIAIdentifiers();
         }
 
         public async Task<ICollection<KBART>>GetKBARTAsync(string urlRoot)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return (await restClient.GetKBARTAsync(urlRoot).ConfigureAwait(false));
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return (await restClient.GetKBARTAsync(urlRoot).ConfigureAwait(false));
         }
 
         public ICollection<KBART> GetKBART(string urlRoot)
         {
-            using (var httpClient = GetHttpClient())
-            {
-                BHLWS restClient = new BHLWS(_baseUrl, httpClient);
-                return restClient.GetKBART(urlRoot);
-            }
+            BHLWS restClient = new BHLWS(_baseUrl, _httpClient);
+            return restClient.GetKBART(urlRoot);
         }
     }
 }
