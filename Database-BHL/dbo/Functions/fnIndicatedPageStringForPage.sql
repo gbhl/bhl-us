972a254a9d10982fc5e17d1efce515d6dc34837c
mlichtenberg <mlichtenberg@hotmail.com>
2022-12-29 16:42:31 -0600

    Rewrote fnIndicatedPageStringForPage to correctly handle data with special chars

--- Database-BHL/dbo/Functions/fnIndicatedPageStringForPage.sql
+++ Database-BHL/dbo/Functions/fnIndicatedPageStringForPage.sql
@@ -8,18 +8,21 @@ AS
 BEGIN
 	
 	DECLARE @IndicatedPageString nvarchar(max)
+	DECLARE @CurrentRecord int
+	SELECT @CurrentRecord = 1
 
-	SELECT @IndicatedPageString = STUFF((
-	
-		SELECT ',' + ip.PagePrefix + ' ' + 
-					(CASE WHEN ip.Implied = 1 THEN '[' + ip.PageNumber + ']' ELSE ip.PageNumber END)
-		FROM Page p
-		INNER JOIN IndicatedPage ip ON (p.PageID = ip.PageID)
-		WHERE p.PageID = @PageID
-		ORDER BY ip.Sequence ASC
-		FOR XML PATH('')
-		),1,1,'')
+	SELECT 
+		@IndicatedPageString = COALESCE(@IndicatedPageString, '') +
+			(CASE WHEN @CurrentRecord = 1 THEN '' ELSE ', ' END) + 
+			ip.PagePrefix + ' ' + 
+			ISNULL((CASE WHEN ip.Implied = 1 THEN '[' + ip.PageNumber + ']' ELSE ip.PageNumber END), ''),
+		@CurrentRecord = @CurrentRecord + 1
+	FROM dbo.Page p
+	INNER JOIN dbo.IndicatedPage ip ON (p.PageID = ip.PageID)
+	WHERE p.PageID = @PageID
+	ORDER BY ip.Sequence ASC
 
 	RETURN SUBSTRING(LTRIM(RTRIM(COALESCE(@IndicatedPageString, ''))), 1, 1024)
 END
 
+GO
