86078820209ed095d0e363b5819c5813f508cc2e
mlichtenberg <mlichtenberg@hotmail.com>
2023-11-16 15:14:29 -0600

    Removed inactive code from PageSummaryService module

--- BHLUSWeb2/services/PageSummaryService.ashx.cs
+++ BHLUSWeb2/services/PageSummaryService.ashx.cs
@@ -1,12 +1,8 @@
-using BHL.SiteServiceREST.v1.Client;
-using BHL.SiteServicesREST.v1;
 using MOBOT.BHL.DataObjects;
-using MOBOT.BHL.DataObjects.Enum;
 using MOBOT.BHL.Server;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Script.Serialization;
@@ -14,44 +10,23 @@ using System.Web.Services;
 
 namespace MOBOT.BHL.Web2.Services
 {
-    /// <summary>
-    /// Summary description for $codebehindclassname$
-    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     public class PageSummaryService1 : IHttpHandler
     {
         public void ProcessRequest(HttpContext context)
         {
-            String response;
+            string response;
 
             // Clean up inputs
-            String pageTypeID = context.Request.QueryString["pageTypeID"] as String;
-            String titleID = context.Request.QueryString["titleID"] as String;
-            String pageID = context.Request.QueryString["pageID"] as String;
-            String itemID = context.Request.QueryString["itemID"] as String;
-            pageTypeID = String.IsNullOrEmpty(pageTypeID) ? "0" : pageTypeID;
-            titleID = String.IsNullOrEmpty(titleID) ? "0" : titleID;
-            pageID = String.IsNullOrEmpty(pageID) ? "0" : pageID;
-            itemID = String.IsNullOrEmpty(itemID) ? "0" : itemID;
+            string pageID = context.Request.QueryString["pageID"] as string;
+            pageID = string.IsNullOrEmpty(pageID) ? "0" : pageID;
 
             switch (context.Request.QueryString["op"])
             {
-                case "FetchPageUrl":
-                    {
-                        response = this.FetchPageUrl(context,
-                                            Convert.ToInt32(pageID));
-                        break;
-                    }
                 case "GetPageNameList":
                     {
-                        response = this.GetPageNameList(
-                                            Convert.ToInt32(pageID));
-                        break;
-                    }
-                case "PageSummarySelectForViewerByItemID":
-                    {
-                        response = this.PageSummarySelectForViewerByItemID(itemID);
+                        response = this.GetPageNameList(Convert.ToInt32(pageID));
                         break;
                     }
                 case "GetPageOcrText":
@@ -68,15 +43,6 @@ namespace MOBOT.BHL.Web2.Services
 
             context.Response.ContentType = "application/json";
             context.Response.Write(response);
-        }
-
-        private string FetchPageUrl(HttpContext context, int pageID)
-        {
-            PageSummaryService pss = new PageSummaryService();
-            string[] sa = pss.FetchPageUrl(pageID);
-
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            return js.Serialize(sa);
         }
 
         private string GetPageOcrText(int pageID)
@@ -108,7 +74,6 @@ namespace MOBOT.BHL.Web2.Services
 
         private string GetPageNameList(int pageID)
         {
-            this.PopulatePageNames(pageID);
             List<NameResolved> namePageList = new BHLProvider().NameResolvedSelectByPageID(pageID);
             List<NameResolved> returnList = new List<NameResolved>();
             foreach (NameResolved namePage in namePageList)
@@ -122,92 +87,6 @@ namespace MOBOT.BHL.Web2.Services
 
             JavaScriptSerializer js = new JavaScriptSerializer();
             return js.Serialize(returnList);
-        }
-
-        /// <summary>
-        /// Uses the OCR for the page to look up any names that weren't previously identified by SciLINC
-        /// </summary>
-        /// <param name="itemID"></param>
-        /// <param name="sequenceOrder"></param>
-        private void PopulatePageNames(int pageID)
-        {
-            BHLProvider provider = new BHLProvider();
-            Page page = provider.PageSelectAuto(pageID);
-            bool doLookup = false;
-
-            // Check the configuration setting to make sure we're looking up names remotely
-            if (ConfigurationManager.AppSettings["DoRemoteNameLookup"] == "true")
-            {
-                // Look up the page names if we never have for this page, or if it's been longer
-                // than the maximum page name age since we've looked them up
-                if (page.LastPageNameLookupDate == null)
-                {
-                    doLookup = true;
-                }
-                else
-                {
-                    TimeSpan ts = DateTime.Now.Subtract((DateTime)page.LastPageNameLookupDate);
-                    if (ts.Days > Convert.ToInt32(ConfigurationManager.AppSettings["MaximumPageNameAge"]))
-                        doLookup = true;
-                }
-            }
-
-            if (doLookup)
-            {
-                try
-                {
-                    List<NameFinderResponse> names = new PageSummaryService().GetNamesFromOcr(pageID);
-                    provider.PageNameUpdateList(pageID, names, ConfigurationManager.AppSettings["NameServiceSourceName"]);
-                    provider.PageUpdateLastPageNameLookupDate(page.PageID);
-                }
-                catch (System.Net.WebException wex)
-                {
-                    // Catch and ignore web exceptions, as they likely represent a
-                    // communication failure with the uBio service.
-                }
-                catch
-                {
-                }
-            }
-        }
-
-        private string PageSummarySelectForViewerByItemID(string itemIDString)
-        {
-            try
-            {
-                int itemID;
-                if (Int32.TryParse(itemIDString, out itemID))
-                {
-                    List<PageSummaryView> pages = new BHLProvider().PageSummarySelectForViewerByItemID(itemID);
-
-                    // Serialize only the information we need
-                    List<ViewerPageModel> viewerPages = new List<ViewerPageModel>();
-                    foreach (PageSummaryView page in pages)
-                    {
-                        ViewerPageModel viewerPage = new ViewerPageModel();
-                        viewerPage.ExternalBaseUrl = page.ExternalBaseURL;
-                        viewerPage.AltExternalUrl = page.ExternalURL;
-                        viewerPage.BarCode = page.BarCode;
-                        viewerPage.SequenceOrder = page.SequenceOrder;
-                        viewerPages.Add(viewerPage);
-                    }
-
-                    // Add the height and width of each page to the list
-                    Client client = new Client(ConfigurationManager.AppSettings["SiteServicesURL"]);
-                    viewerPages = client.GetItemPageImageDimensions(itemID, viewerPages).ToList<ViewerPageModel>();
-
-                    JavaScriptSerializer js = new JavaScriptSerializer();
-                    return js.Serialize(viewerPages);
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch
-            {
-                return null;
-            }
         }
 
         public bool IsReusable
--- BHLUSWeb2/services/PageSummaryService.asmx
+++
@@ -1,1 +0,0 @@
-<%@ WebService Language="C#" CodeBehind="PageSummaryService.asmx.cs" Class="MOBOT.BHL.Web2.Services.PageSummaryService" %>
