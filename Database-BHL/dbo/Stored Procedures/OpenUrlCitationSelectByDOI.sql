f804c29a6769e703dd750a2e3c6c1840b2310945
mlichtenberg <mlichtenberg@hotmail.com>
2025-01-23 16:34:58 -0600

    Updates to stored procedures that had been failing unit tests

--- Database-BHL/dbo/Stored Procedures/OpenUrlCitationSelectByDOI.sql
+++ Database-BHL/dbo/Stored Procedures/OpenUrlCitationSelectByDOI.sql
@@ -11,13 +11,20 @@ SET NOCOUNT ON
 DECLARE @DOIEntityTypeName nvarchar(50)
 DECLARE @EntityID int
 
--- Get the entity details for the DOI	
-SELECT	@DOIEntityTypeName = et.DOIEntityTypeName,
-		@EntityID = d.EntityID
-FROM	dbo.DOI d INNER JOIN dbo.DOIEntityType et
-			ON d.DOIEntityTypeID = et.DOIEntityTypeID
-WHERE	d.DOIName = @DOIName
-AND		d.IsValid = 1
+-- Get the entity details for the DOI
+SELECT	@DOIEntityTypeName = 'Title',
+		@EntityID = ti.TitleID
+FROM	dbo.Title_Identifier ti
+WHERE	IdentifierValue = @DOIName
+
+IF (@DOIEntityTypeName IS NULL)
+BEGIN
+	SELECT	@DOIEntityTypeName = 'Segment',
+			@EntityID = s.SegmentID
+	FROM	dbo.ItemIdentifier ii
+			INNER JOIN dbo.Segment s ON ii.ItemID = s.ItemID
+	WHERE	IdentifierValue = @DOIName
+END
 
 -- Call the appropriate OpenUrl Citation resolver for the entity type
 IF @DOIEntityTypeName = 'Title' exec dbo.OpenUrlCitationSelectByTitleID @EntityID
