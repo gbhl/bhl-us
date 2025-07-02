aac1725bd637eb23b0eb714b8e495fbb0c41bfaf
mlichtenberg <mlichtenberg@hotmail.com>
2025-07-02 18:04:20 -0500

    Updates to webstats

--- BHLAdminWeb/Models/WebStatsModel.cs
+++ BHLAdminWeb/Models/WebStatsModel.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 
 namespace MOBOT.BHL.AdminWeb.Models
 {
     public class WebStatsModel
     {
         string _appPath;
+        public SortedList<string, string> statusList = new SortedList<string, string>();
+        public SortedList<string, string> stemList = new SortedList<string, string>();
         public List<WebStat> overallTraffic { get; set; } = new List<WebStat>();
-        public List<WebStat> statusTraffic { get; set; } = new List<WebStat>();
-        public List<WebStat> stemTraffic { get; set; } = new List<WebStat>();
+        public SortedDictionary<DateTime, SortedDictionary<string, int>> statusTraffic { get; set; } = new SortedDictionary<DateTime, SortedDictionary<string, int>>();
+        public SortedDictionary<DateTime, SortedDictionary<string, int>> stemTraffic { get; set; } = new SortedDictionary<DateTime, SortedDictionary<string, int>>();
         public List<WebStat> stemstatusTraffic { get; set; } = new List<WebStat>();
 
         public WebStatsModel(string appPath)
@@ -21,12 +24,14 @@ namespace MOBOT.BHL.AdminWeb.Models
         public void GetWebStats()
         {
             GetTrafficStatsFromLogFile(new List<string> { ConfigurationManager.AppSettings["DailyStatsServer1"], ConfigurationManager.AppSettings["DailyStatsServer2"] }, overallTraffic);
+            GetStatusTrafficStatsFromLogFile(new List<string> { ConfigurationManager.AppSettings["DailyByStatusStatsServer1"], ConfigurationManager.AppSettings["DailyByStatusStatsServer2"] }, statusTraffic);
+            GetStemTrafficStatsFromLogFile(new List<string> { ConfigurationManager.AppSettings["DailyByStemStatsServer1"], ConfigurationManager.AppSettings["DailyByStemStatsServer2"] }, stemTraffic);
         }
 
         private void GetTrafficStatsFromLogFile(List<string> logFiles, List<WebStat> trafficStats)
         {
             // Combine the stats from the log files
-            Dictionary<DateTime, int> hourlyStats = new Dictionary<DateTime, int>();
+            Dictionary<DateTime, int> dailyStats = new Dictionary<DateTime, int>();
             foreach (string logFile in logFiles)
             {
                 string[] logLines = File.ReadAllLines(_appPath + logFile);
@@ -39,27 +44,165 @@ namespace MOBOT.BHL.AdminWeb.Models
                     }
                     else
                     {
-                        string[] hourlyStat = logLine.Split(',');
-                        DateTime hourlyKey = DateTime.Parse(hourlyStat[0]);
-                        int hourlyRequests = Int32.Parse(hourlyStat[1]);
-                        if (hourlyStats.ContainsKey(hourlyKey))
-                            hourlyStats[hourlyKey] += hourlyRequests;
+                        string[] dailyStat = logLine.Split(',');
+                        DateTime dailyKey = DateTime.Parse(dailyStat[0]);
+                        int dailyRequests = Int32.Parse(dailyStat[1]);
+                        if (dailyStats.ContainsKey(dailyKey))
+                            dailyStats[dailyKey] += dailyRequests;
                         else
-                            hourlyStats.Add(hourlyKey, hourlyRequests);
+                            dailyStats.Add(dailyKey, dailyRequests);
                     }
                 }
             }
 
             // Add the log file stats to the traffic stats
-            foreach (var hourlyStat in hourlyStats)
+            foreach (var dailyStat in dailyStats)
             {
                 trafficStats.Add(new WebStat
                 {
-                    Year = hourlyStat.Key.Year,
-                    Month = hourlyStat.Key.Month,
-                    Day = hourlyStat.Key.Day,
-                    Requests = hourlyStat.Value,
+                    Year = dailyStat.Key.Year,
+                    Month = dailyStat.Key.Month,
+                    Day = dailyStat.Key.Day,
+                    Requests = dailyStat.Value,
                 });
+            }
+        }
+
+        private void GetStatusTrafficStatsFromLogFile(List<string> logFiles, SortedDictionary<DateTime, SortedDictionary<string, int>> trafficStats)
+        {
+            // Combine the stats from the log files
+            List<DateTime> dates = new List<DateTime>();
+            Dictionary<string, int> statusStats = new Dictionary<string, int>();
+            foreach (string logFile in logFiles)
+            {
+                // First pass through log files to collect all unique date and status values
+                string[] logLines = File.ReadAllLines(_appPath + logFile);
+
+                bool firstLine = true;
+                foreach (string logLine in logLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] dailyStat = logLine.Split(',');
+                        DateTime dailyKey = DateTime.Parse(dailyStat[0]);
+                        string statusKey = dailyStat[1];
+                        if (!dates.Contains(dailyKey)) dates.Add(dailyKey);
+                        if (!statusStats.ContainsKey(statusKey)) statusStats.Add(statusKey, 0);
+                    }
+                }
+            }
+
+            // Get the list of unique statuses
+            foreach(var s in statusStats) statusList.Add(s.Key, s.Key);
+
+            // Add the dictionary of all possible status values to each date in the dictionary of all dates
+            foreach (DateTime date in dates)
+            {
+                if (!trafficStats.ContainsKey(date)) trafficStats.Add(date, new SortedDictionary<string, int>(statusStats));
+            }
+
+            foreach (string logFile in logFiles)
+            {
+                // Second pass through the log files to assign values to each element in the dictionary of traffic
+                string[] logLines = File.ReadAllLines(_appPath + logFile);
+
+                bool firstLine = true;
+                foreach (string logLine in logLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] dailyStat = logLine.Split(',');
+                        DateTime dailyKey = DateTime.Parse(dailyStat[0]);
+                        string statusKey = dailyStat[1];
+                        int statusRequests = Int32.Parse(dailyStat[2]);
+                        trafficStats[dailyKey][statusKey] += statusRequests;
+                    }
+                }
+            }
+        }
+
+        private void GetStemTrafficStatsFromLogFile(List<string> logFiles, SortedDictionary<DateTime, SortedDictionary<string, int>> trafficStats)
+        {
+            // Combine the stats from the log files
+            List<DateTime> dates = new List<DateTime>();
+            Dictionary<string, int> stemStats = new Dictionary<string, int>();
+            foreach (string logFile in logFiles)
+            {
+                // First pass through log files to collect all unique date and status values
+                string[] logLines = File.ReadAllLines(_appPath + logFile);
+
+                bool firstLine = true;
+                foreach (string logLine in logLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] dailyStat = logLine.Split(',');
+                        DateTime dailyKey = DateTime.Parse(dailyStat[0]);
+                        string stemKey = dailyStat[1].Trim();
+                        if (stemKey != "/cgi-bin" &&
+                            stemKey != "/robots.txt" &&
+                            stemKey != "/scripts" &&
+                            stemKey != "/wp-admin" &&
+                            stemKey != "/wp-content" &&
+                            stemKey != "/wp-includes")
+                        {
+                            if (!dates.Contains(dailyKey)) dates.Add(dailyKey);
+                            if (!stemStats.ContainsKey(stemKey)) stemStats.Add(stemKey, 0);
+                        }
+                    }
+                }
+            }
+
+            // Get the list of unique stems
+            foreach (var s in stemStats) stemList.Add(s.Key, s.Key);
+
+            // Add the dictionary of all possible stem values to each date in the dictionary of all dates
+            foreach (DateTime date in dates)
+            {
+                if (!trafficStats.ContainsKey(date)) trafficStats.Add(date, new SortedDictionary<string, int>(stemStats));
+            }
+
+            foreach (string logFile in logFiles)
+            {
+                // Second pass through the log files to assign values to each element in the dictionary of traffic
+                string[] logLines = File.ReadAllLines(_appPath + logFile);
+
+                bool firstLine = true;
+                foreach (string logLine in logLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] dailyStat = logLine.Split(',');
+                        DateTime dailyKey = DateTime.Parse(dailyStat[0]);
+                        string stemKey = dailyStat[1].Trim();
+                        int stemRequests = Int32.Parse(dailyStat[2]);
+                        if (stemKey != "/cgi-bin" &&
+                            stemKey != "/robots.txt" &&
+                            stemKey != "/scripts" &&
+                            stemKey != "/wp-admin" &&
+                            stemKey != "/wp-content" &&
+                            stemKey != "/wp-includes")
+                        {
+                            trafficStats[dailyKey][stemKey] += stemRequests;
+                        }
+                    }
+                }
             }
         }
 
@@ -68,15 +211,11 @@ namespace MOBOT.BHL.AdminWeb.Models
             int _year;
             int _month;
             int _day;
-            string _status;
-            string _stem;
             int _requests;
 
             public int Year { get => _year; set => _year = value; }
             public int Month { get => _month; set => _month = value; }
             public int Day { get => _day; set => _day = value; }
-            public string Status { get => _status; set => _status = value; }
-            public string Stem { get => _stem; set => _stem = value; }
             public int Requests { get => _requests; set => _requests = value; }
         }
     }
