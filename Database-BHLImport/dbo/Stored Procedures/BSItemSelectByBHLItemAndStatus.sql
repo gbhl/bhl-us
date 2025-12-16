c084a035578b74fab3f87aba450a80c98b0a9741
mlichtenberg <mlichtenberg@hotmail.com>
2025-12-16 16:27:24 -0600

    Added BHLItemID to the result set for BSItemSelectByBHLItemAndStatus

--- Database-BHLImport/dbo/Stored Procedures/BSItemSelectByBHLItemAndStatus.sql
+++ Database-BHLImport/dbo/Stored Procedures/BSItemSelectByBHLItemAndStatus.sql
@@ -9,7 +9,7 @@ BEGIN
 
 SET NOCOUNT ON
 
-SELECT	i.ItemID
+SELECT	i.ItemID, i.BHLItemID
 FROM	dbo.BSItem i
 WHERE	i.ItemStatusID = @ItemStatusID
 AND		(i.BHLItemID = @BHLItemID OR @BHLItemID IS NULL)
